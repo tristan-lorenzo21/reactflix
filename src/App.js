d954ca041e986fb48de216ef11f99cb63fe859c7
tristan-lorenzo21 <tristan.lorenzo21@gmail.com>
2020-11-23 22:20:14 -0800

    Updated search function to display results as search bar is used

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
-import './App.css'
+import React, { Component } from "react";
+import axios from "axios";
 
-import SearchBar from './components/SearchBar'
+import { search } from "./utils";
+import Movies from "./Movies";
 
-export default function App() {
-  return (
-    <SearchBar />
-  )
+class App extends Component {
+  state = {
+    movies: null,
+    loading: false,
+    value: ""
+  };
+
+  search = async val => {
+    this.setState({ loading: true });
+
+    //23d2860375da167fa19998ec1ba7feae
+
+    const results = await search(
+      `https://api.themoviedb.org/3/search/movie?query=${val}&api_key=23d2860375da167fa19998ec1ba7feae`
+    );
+    const movies = results;
+
+    this.setState({ movies, loading: false });
+  };
+
+  onChangeHandler = async e => {
+    this.search(e.target.value);
+    this.setState({ value: e.target.value });
+  };
+
+  get renderMovies() {
+    let movies = <h1>Search for a movie!</h1>;
+    if (this.state.movies) {
+      movies = <Movies list={this.state.movies} />;
+    }
+
+    return movies;
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.value}
+          onChange={e => this.onChangeHandler(e)}
+          placeholder="Type something to search"
+        />
+        {this.renderMovies}
+      </div>
+    );
+  }
 }
+
+export default App;
